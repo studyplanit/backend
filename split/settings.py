9399fefd81328632cc9975378b51d722fb2f2271
skwls2087 <63686155+skwls2087@users.noreply.github.com>
2020-10-13 02:01:42 +0900

    db설정 변경

--- split/settings.py
+++ split/settings.py
@@ -75,7 +75,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'split',
         'USER' : 'root',
-        'PASSWORD' : '1234',
+        'PASSWORD' : 'lzsh05a05!',
         'HOST' : '127.0.0.1',
         'PORT' : '3306'
     }
